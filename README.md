4e20b5ff0e616d1831465404645cce7813146bd2
Boldark <apaokagiashraf@gmail.com>
2022-11-16 07:50:35 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Documentation fo simple shell group project
---
+++ atoi.c
@@ -0,0 +1,70 @@
+#include "shell.h"
+
+/**
+ * interactive - Functions that returns true if shell is interactive mode
+ * @info: struct address
+ * Return: 1 (Success) 0 (Failure)
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - Function that checks if character is a delimeter
+ * @c: Character
+ * @delim: the delimeter string
+ * Return: 1 (Success) 0 (Failure)
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
+ *_isalpha - function that checks for alphabetic character
+ *@c: Character
+ *Return: 1 (Success) 0 (Failure)
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ *_atoi - cfunction that onverts a string to an integer
+ *@s: string
+ *Return: 0 (Failure)
+ */
+
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+	return (output);
+}
