6a69f10976ccb74cad557282bf1603403ab06bf6
Boldark <apaokagiashraf@gmail.com>
2022-11-17 15:24:41 -0800

    Completed task

--- builtin1.c
+++ builtin1.c
@@ -1,4 +1,14 @@
 #include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
 
 /**
  * _myhistory - Function that displays the history list
